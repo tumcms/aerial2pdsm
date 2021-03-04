67e93059ca5967a70052af440113818c54836ac5
no68ged <felix.eickeler@tum.de>
2021-03-04 14:23:42 +0100

    populator.py

--- submodules/populator.py
+++ submodules/populator.py
@@ -1,4 +1,3 @@
-from gitdir.gitdir import download as get_subdirectory
-
-colmap_url =  r"https://github.com/colmap/colmap/tree/78b6ae707e79542d34729e95052c10f6310c050b"
-get_subdirectory(colmap_url, flatten=True, r"./colmap_scripts")
+from gitdir.gitdir.gitdir import download as get_subdirectory
+colmap_url = r"https://github.com/colmap/colmap/tree/78b6ae707e79542d34729e95052c10f6310c050b/scripts/python"
+get_subdirectory(colmap_url, flatten=True, output_dir=r"./colmap_scripts")
