57e84b122f71b7812650f190e141d5236744c15f
no68ged <felix.eickeler@tum.de>
2021-03-06 10:56:21 +0100

    optical fixes

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from survey.geo_reference_information import CameraPoseGeoReferenceInformation
 from survey.survey import Survey
 from survey.commands import CreateLocalModels, CreateGlobalModel, ExtendDataBase, IsolateConstructionSites
 from data_io import decoder_setup as serialization
-import argparse    
+import argparse
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='This tools prototype creates a complete resconstruction of an area as used (or it was applied) in the paper ...')
     parser.add_argument("-src", "--source_path", required=True, type=str, help="Folder of all images, with sub_folders")
-    parser.add_argument("-svy", "--survey_path",  required=True, type=str, help="Folder to create the full survey. Please check free memory it will probably be huge")
-    parser.add_argument("-kpts", "--bounding_box_corners", required=True, type=str, help="List of all bounding boxes that should be considered. One image AND keypoint per line, two keypoints (== two lines) are needed for every bounding box. See example files for more information")
+    parser.add_argument("-svy", "--survey_path", required=True, type=str, help="Folder to create the full survey. Please check free memory it will probably be huge")
+    parser.add_argument("-kpts", "--bounding_box_corners", required=True, type=str,
+                        help="List of all bounding boxes that should be considered. One image AND keypoint per line, two keypoints (== two lines) are needed for every bounding box. See example files for more information")
     parser.add_argument("-gd", "--global_dense", action='store_true', help="Recursively walk all subdirectories", )
 
     # extract paths
@@ -21,7 +22,7 @@ if __name__ == "__main__":
 
     survey = None
     # Check if survey exists else create new
-    try:        
+    try:
         survey = Survey.LoadFromJson(survey_path + "/survey.ini")
     except:
         geo_info = CameraPoseGeoReferenceInformation()
